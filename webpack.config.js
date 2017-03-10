b08eb6bf470fdd9f3b4d673dc3c14745864c91be
Amit Ranan <ranan.amit@gmail.com>
2017-03-09 21:42:13 -0800

    installed yarn

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  entry: './public',
+  output: {
+    path: './public',
+    filename: 'bundle.js'
+  },
+  module: {
+      loaders: [
+          {
+            test: /\.(es6|jsx?)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+              presets: ['es2015', 'react', 'stage-0']
+            }
+          },
+          {
+            test: /\.less$/,
+            loader: "style!css!less"
+          },
+          { test: /\.css$/,
+            loader: "style!css"
+          },
+        ]
+      },
+    resolve: {
+    extensions: ["", ".js", ".jsx", ".es6"] // allow you to not include file type when requiring
+    }
+};
