7cfe18e7fbc8bdc7337f1ffc00e5d23ef4db4b0e
Amit Ranan <ranan.amit@gmail.com>
2017-03-11 18:04:04 -0800

    added state to App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,19 @@ import RightPanel from '../RightPanel/RightPanel';
 import './app.scss';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      timeOnline: '',
+      rooms: [],
+      currentRoom: {},
+      messages: []
+    };
+  }
+
+
   render() {
     return (
       <div className="app-container">
