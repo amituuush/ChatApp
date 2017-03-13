7c45d857c71ee80a6516092fc79666dd845121c9
Amit Ranan <ranan.amit@gmail.com>
2017-03-13 13:29:05 -0700

    changed babel to babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
           {
             test: /\.(es6|jsx?)$/,
             exclude: /node_modules/,
-            loader: 'babel',
+            loader: 'babel-loader',
             query: {
               presets: ['es2015', 'react', 'stage-0']
             }
